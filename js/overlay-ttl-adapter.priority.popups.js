60a011188db2f42cac79402244f2775d117fd02b
Movie-playlist-2 <161195477+Movie-playlist-2@users.noreply.github.com>
2026-05-24 17:20:14 +0200

    Update channels

--- js/overlay-ttl-adapter.priority.popups.js
+++ js/overlay-ttl-adapter.priority.popups.js
@@ -121,6 +121,7 @@
         path === "/pages/switchcm.html" ||
         path === "/pages/canal-panda.html" ||
         path === "/pages/disney-pixar-work.html" ||
+        path === "https://streaming-live.rtp.pt/liverepeater/smil:rtp1HD.smil/playlist.m3u8" ||
         path === "/pages/CM-vavoo.html"
     );
     } catch {
