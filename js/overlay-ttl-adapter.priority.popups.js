5e41941a508a62a3aba687f02199fd17e5df7e93
Movie-playlist-2 <161195477+Movie-playlist-2@users.noreply.github.com>
2026-05-31 22:48:48 +0200

    Update files

--- js/overlay-ttl-adapter.priority.popups.js
+++ js/overlay-ttl-adapter.priority.popups.js
@@ -119,11 +119,10 @@
         path === "/pages/iframecm.html" ||
         path === "/pages/cstar.html" ||
         path === "/pages/switchcm.html" ||
-        path === "/pages/canal-panda.html" ||
-        path === "/pages/disney-pixar-work.html" ||
-        path === "/pages/cm-tv247.html" ||
-        path === "/https://chat.cfbu247.sbs/api/proxy/player?token=eyJjaGFubmVsSWQiOiI3OTAiLCJ0cyI6MTc4MDA2NzU3MjU3OH0" ||
-        path === "/pages/CM-vavoo.html"
+        path === "/" ||
+        path === "/" ||
+        path === "/" ||
+        path === "/"
     );
     } catch {
       return false;
