afce0b001e6530327d6c150af68b75aa948fa549
Movie-playlist-2 <161195477+Movie-playlist-2@users.noreply.github.com>
2026-06-27 10:08:17 +0200

    auto deploy: 2026-06-27 10:08:17

--- worker.js
+++ worker.js
@@ -1,5 +1,23 @@
 export default {
   async fetch(request, env) {
-    return env.ASSETS.fetch(request);
+    const url = new URL(request.url);
+    const response = await env.ASSETS.fetch(request);
+
+    if (url.pathname === '/pages/cmtvpt' || url.pathname === '/pages/cmtvpt.html') {
+      const headers = new Headers(response.headers);
+      headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0');
+      headers.set('CDN-Cache-Control', 'no-store');
+      headers.set('Cloudflare-CDN-Cache-Control', 'no-store');
+      headers.set('Pragma', 'no-cache');
+      headers.set('Expires', '0');
+
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers
+      });
+    }
+
+    return response;
   }
 };
