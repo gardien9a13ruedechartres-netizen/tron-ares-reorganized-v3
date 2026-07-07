2ecba2834afe2b1b0da3ac72f31d33c58ff7722b
Movie-playlist-2 <161195477+Movie-playlist-2@users.noreply.github.com>
2026-07-07 21:49:55 +0200

    Update files

--- js/overlay-ttl-adapter.priority.popups.js
+++ js/overlay-ttl-adapter.priority.popups.js
@@ -129,7 +129,7 @@
         path === "/pages/rtp1" ||
         path === "/pages/rtp2.html" ||
         path === "/pages/rtp2" ||
-        path === "/pages/serie-film?type=external&provider" ||
+        path === "/pages/serie-film?q=matrix" ||
         path === "/"
     );
     } catch {
